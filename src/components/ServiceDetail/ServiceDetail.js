b40bcec5d3a99ba0796c81725912e62f9d290f47
Md. Moinul Hassan <moinulh.msn@gmail.com>
2022-11-10 18:53:32 +0600

    added service detail route

--- src/components/ServiceDetail/ServiceDetail.js
+++ src/components/ServiceDetail/ServiceDetail.js
@@ -2,13 +2,17 @@ import React, { useEffect } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const ServiceDetail = () => {
-    const {name} = useLoaderData();
+    const {name, img, details, price } = useLoaderData();
     useEffect(() => {
         document.title = name
     },[name])
+
     return (
-        <div>
-            This is {name}
+        <div className='min-h-screen my-10'>
+            <h2 className='text-5xl my-10'>{name}</h2>
+            <img src={img} className='lg:w-3/4 rounded-xl mx-auto mb-10' alt=''/>
+            <p className='text-start text-3xl font-bold my-10'> Price: {price}</p>
+            <h2 className='text-2xl my-10'>{details}</h2>
         </div>
     );
 };
